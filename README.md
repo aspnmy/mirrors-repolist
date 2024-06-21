f191434ae9ca3524e9a01d650a00b47c5e64219f
aspnmy <support@e2bank.cn>
2024-06-22 02:47:15 +0800

    更新自动脚本工具

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# mirrors-repolist
+#[TOC]
+# 如何优雅的换国内源？
+- 由于国内源的一些历史问题，所以有时候经常会出现换源异常问题，所以这个项目存在的意义就是，缓存一批常用的repo国内源，定期更新，使用很easy，只需要git下来，挪到指定区域即可。
+
+# 使用说明
+- 1、首先下载项目
+```bash
+mkdir -p $HOMME/tmp/downloads
+git clone https://gitcode.com/aspnmy/mirrors-repolist.git
+
+```
+- 2、备份官方源并清空
+```bash
+mkdir -p $HOMME/tmp/backup
+cp -r /etc/yum.repos.d/*.repo $HOMME/tmp/backup
+rm -rf /etc/yum.repos.d/*.repo 
+
+```
+
+- 3、拷贝需要的repo文件到/etc/yum.repos.d/，并重建缓存
+    比如需要项目中的centos-9-stream下全部常用repo，就可以用下面代码拷贝进去
+```base
+mkdir -p $HOMME/tmp/backup
+cp -r $HOMME/tmp/downloads/mirrors-repolist/aliyun/centos/9-stream/*.repo /etc/yum.repos.d
+dnf makecache
+
+```
+
+# 使用sh脚本定期换源
+- 为什么要定期换源呢，因为某些原因国内的镜像源可能会有访问异常，所以git上的数据会有变动
+- 假设git到本地的目录是$HOMME/tmp/downloads，是不变的，那么写一个简单sh脚本，定期git到本地，然后再拷贝到/etc/yum.repos.d/目录下就能无缝衔接源了，可以用定时组件操作。
+- 脚本工具在git项目的./mirrors-repolist/sh/下
+
+```base
+# 安装脚本工具到系统目录 （如果不使用install命令进行安装，就需要以./mirrors-repolist/sh/autoRepoGit.sh start 的形式来启动）
+autoRepoGit install
+# 自动更新一次repo  
+autoRepoGit start
+# 自动备份repo文件到备份目录 
+autoRepoGit backup
+# 设置定时任务为每月第一天执行 
+autoRepoGit set_crontab
+
+
+```
+
+## mirrors-repolist
 
 - 主要保存一些常用镜像源的repo文件，方便快速换源
 - 使用wget命令下载的对应区域即可
---
+++ sh/autoRepoGit.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# 自动更新一次repo  autoRepoGit start
+# 自动备份repo文件到备份目录 autoRepoGit backup
+# 安装脚本工具到系统目录 autoRepoGit install
+# 设置定时任务为每月第一天执行 autoRepoGit set_crontab
+# git：https://gitcode.com/aspnmy/mirrors-repolist.git
+# 作者：aspnmy（support@e2bank.cn）
+# 版本更新：20240622
+
+
+# 只支持root用户操作，如果需要使用非root用户需要脚本每个命令中都加上sudo 不然执行是不成功的
+# 执行脚本前需要确认 依赖组件 git  已经安装完成
+# 如果需要定时业务 确认依赖组件 crontabs 已经安装完成
+USERNAME="root"
+SCNAME="autoRepoGit"
+# $Barce_VER 代表git目录中需要下载的repo文件的类别路径，用变量就方便进行路径切换，这里假设拉取阿里云-centos-9-stream的repo文件
+Barce_VER="aliyun/centos/9-stream"
+DL_PATH="$HOMME/tmp/downloads"
+RCOPY_PATH="$DL_PATH/mirrors-repolist/$Barce_VER"
+SERVICE="autoRepoGit"
+#$REPO_DIR 就是 /etc/yum.repos.d/目录
+REPO_DIR="/etc/yum.repos.d"
+BACK_DIR="$HOMME/tmp/backup"
+ 
+
+ME=`whoami`
+ 
+if [ $ME != $USERNAME ] ; then
+   echo "Please run the $USERNAME user."
+   exit
+fi
+ 
+startOnce() {
+   if pgrep -u $USERNAME -f $SERVICE > /dev/null ; then
+       echo "$SERVICE is already running!"
+       exit
+   fi
+    cd $DL_PATH
+    git clone -q https://gitcode.com/aspnmy/mirrors-repolist.git
+    cp -r -y $RCOPY_PATH/*.repo $REPO_DIR
+    dnf makecache
+   exit
+}
+ 
+back_repo() {
+   if pgrep -u $USERNAME -f $SERVICE > /dev/null ; then
+       echo "back_repoiing $SERVICE "
+   else
+       echo "$SERVICE is not running!"
+       exit
+   fi
+ 
+   cp -r -y $REPO_DIR/*.repo $BACK_DIR
+   exit
+}
+ 
+install_unit() {
+    # 安装脚本到系统目录下
+    # 默认脚本路径在 ./mirrors-repolist/sh/autoRepoGit.sh
+ 
+   cp -r -y ./mirrors-repolist/sh/autoRepoGit.sh /usr/bin/autoRepoGit
+   chmod +x /usr/bin/autoRepoGit
+   systemctl enable crond
+   systemctl start crond
+   echo "$SERVICE is Succeed!"
+   exit
+}
+
+
+set_crontab_mouth() {
+    # 设置定时任务一个月执行一次
+    # 默认脚本路径在 ./mirrors-repolist/sh/autoRepoGit.sh
+ 
+   cat >> /etc/crontab << 'EOF'
+# 将当前时间写入到log文件,每个月的1号执行一次
+0 0 1 * * autoRepoGit start echo `date` >> /root/date.log
+EOF
+
+crontab /etc/crontab
+systemctl enable crond
+systemctl reload crond
+   echo "Crontab is Succeed!"
+
+   exit
+}
+
+case "$1" in
+   start)
+       startOnce
+       ;;
+   backup)
+       back_repo
+       ;;
+   install)
+       install_unit
+       ;;   
+   set_crontab)
+       set_crontab_mouth
+       ;;        
+   *)
+       echo  $"Usage: $0 {startOnce(更新repo一次)|backup(备份repo)|install(安装脚本到系统目录)|set_crontab(设置定时任务为每月1号)}"
+     
+esac
